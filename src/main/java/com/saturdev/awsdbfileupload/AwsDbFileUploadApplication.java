c19104e28a450b4138654914a0eedbafb44e1cec
Brian-Weloba <bweloba@gmail.com>
2021-11-08 14:01:31 +0300

    Initial Commit

---
+++ src/main/java/com/saturdev/awsdbfileupload/AwsDbFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.saturdev.awsdbfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDbFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDbFileUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saturdev/awsdbfileupload/AwsDbFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saturdev.awsdbfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDbFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
